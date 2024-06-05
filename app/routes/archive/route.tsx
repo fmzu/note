fb861cb75d14859bd63cf2ccdfd7606ff04e8424
fmzu <fjtmzk0723@gmail.com>
2024-06-05 17:07:27 +0900

    update: 余白を修正する

--- app/routes/archive/route.tsx
+++ app/routes/archive/route.tsx
@@ -39,7 +39,7 @@ export default function ArchivePage() {
         <NoteNavigation />
         <Separator style={{ height: "initial" }} orientation="vertical" />
         <div className="w-full">
-          <div className="px-4 space-y-4">
+          <div className="p-4 space-y-4">
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
               {query.data?.map(
                 (post) =>
